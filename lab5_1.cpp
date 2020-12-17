0c2a78e2aa4737ddfcd005e49692d92e93cce513
Purich Seenuallae <purichseenuallae@gmail.com>
2020-12-17 17:50:40 +0700

    lab5_1.cpp completed

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -2,8 +2,30 @@
 using namespace std;
 
 int main(){
+    int input = 1;
+    int Evennumbers = 0;
+    int Oddnumbers = 0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> input;
+    if (input!=0)
+    {
+        while (input!=0)
+    {
+        
+        if (input%2==0)
+        {
+            Evennumbers++;
+        }
+        if (input%2!=0)
+        {
+            Oddnumbers++;
+        }
+        cout << "Enter an integer: ";
+        cin >> input;
+        
+    }
+    }
+    cout << "#Even numbers = " << Evennumbers << "\n";    
+    cout << "#Odd numbers = "<< Oddnumbers;
     return 0;
 }
